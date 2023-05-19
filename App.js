55b79c637c0b808797d5fd7af033b807ef98a193
SajjanKarn <karnaa787@gmail.com>
2023-05-19 17:47:21 +0545

    some intial basic setup

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Riveruta-Bold": require("./assets/fonts/Riveruta-Bold.ttf"),
+    "Riveruta-Regular": require("./assets/fonts/Riveruta-Regular.ttf"),
+    "Riveruta-Medium": require("./assets/fonts/Riveruta-Medium.ttf"),
+    "Riveruta-Light": require("./assets/fonts/Riveruta-Light.ttf"),
+    "Riveruta-Thin": require("./assets/fonts/Riveruta-Thin.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ config/colors.js
@@ -0,0 +1,5 @@
+export default colors = {
+  dark: {
+    primary: "#1A1A1A",
+  },
+};
