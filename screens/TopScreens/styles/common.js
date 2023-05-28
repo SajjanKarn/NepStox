ad72a77b022e674598b35f9aeb7d5f4541e9ea34
SajjanKarn <karnaa787@gmail.com>
2023-05-28 08:45:28 +0545

    refactored top screen common styles

--- screens/TopScreens/styles/common.js
+++ screens/TopScreens/styles/common.js
@@ -7,46 +7,4 @@ export default styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.dark.primary,
   },
-  loader: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  // gainer
-  topGainerTitle: {
-    fontSize: totalSize(2),
-    fontWeight: "bold",
-    marginBottom: height(2),
-  },
-  gainerTableBorder: {
-    borderWidth: 1,
-    borderColor: colors.dark.placeholderText,
-  },
-  gainerTable: {
-    marginBottom: height(2),
-  },
-  head: {
-    height: height(7),
-    textAlign: "center",
-    backgroundColor: colors.dark.secondary,
-  },
-  loserHead: {
-    backgroundColor: colors.dark.topLoserText,
-  },
-  headText: {
-    fontSize: totalSize(1.5),
-    textAlign: "center",
-    fontWeight: "bold",
-    textTransform: "uppercase",
-    fontFamily: "Riveruta-Bold",
-    color: colors.dark.textColor,
-  },
-  text: {
-    padding: totalSize(1.8),
-    fontSize: totalSize(1.4),
-    fontFamily: "Riveruta-Medium",
-    color: colors.dark.textColor,
-    textAlign: "center",
-  },
 });
